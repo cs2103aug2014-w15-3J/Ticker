fed3f084a94e762bbd361c3f918c624a3b917c29
unknown <kexinliu131@gmail.com>
2014-09-26 15:48:04 +0800

    added skeleton for Add Delete Edit Search

--- src/Parser.java
+++ src/Parser.java
@@ -9,6 +9,55 @@ public class Parser {
 		sc = new Scanner(System.in);
 	}
 
+	public void getCommand(){
+		String command = sc.nextLine();
+		while (command.isEmpty())
+			command = sc.nextLine();
+		String[] words = command.split(" ");
+		for (int i=0;i<words.length;i++){
+			words[i] = words[i].trim();
+		}
+	
+		if (words[0].equals("add")){
+			callAdd(words);
+		}
+		
+		if (words[0].equals("delete")){
+			callDelete(words);
+		}
+		
+		if (words[0].equals("search")){
+			callSearch(words);
+		}
+		
+		if (words[0].equals("edit")){
+			callEdit(words);
+		}
+	}
+	
+	private void callAdd(String[] words){
+		
+		
+		
+	}
+	
+	private void callDelete(String[] words){
+		
+		
+		
+	}
+	
+	private void callEdit(String[] words){
+		
+		
+		
+	}
+	
+	private void callSearch(String[] words){
+		
+		
+	
+	}
 	
 	private Time constructTime(String str){
 		int firstIndex = str.indexOf(':');
@@ -31,4 +80,9 @@ public class Parser {
 		return null;
 	}
 	
+	
+	private Date constructDate(String str){
+		return null;
+	}
+
 }
