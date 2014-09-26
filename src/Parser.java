1ec70f8a72e2dd9558a3d048da07a5e56aafbd54
unknown <kexinliu131@gmail.com>
2014-09-26 15:30:53 +0800

    added constructTime() method and part of getCommand()

--- src/Parser.java
+++ src/Parser.java
@@ -1,4 +1,34 @@
+import java.util.Scanner;
+
 
 public class Parser {
 
+	Scanner sc;
+	
+	public Parser(){
+		sc = new Scanner(System.in);
+	}
+
+	
+	private Time constructTime(String str){
+		int firstIndex = str.indexOf(':');
+		int lastIndex = str.lastIndexOf(':');
+		
+		if (firstIndex == lastIndex){
+			for (int i=0;i<str.length();i++){
+				if (i!=firstIndex&&(str.charAt(i)<'0'||str.charAt(i)>'9'))
+					return null;
+			}
+			
+			if (firstIndex!=0&&firstIndex!=str.length()-1){
+				int hour = Integer.parseInt(str.substring(0,firstIndex));
+				int minute = Integer.parseInt(str.substring(firstIndex+1));
+				if (hour<24&&minute<60)
+					return new Time(hour,minute); 
+			}
+		}
+		
+		return null;
+	}
+	
 }
