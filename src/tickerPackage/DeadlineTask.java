4537534f24c2c15a93c2c4864607cecd95a72b6d
Nicholette <nekonekonik@gmail.com>
2014-09-28 16:18:10 +0800

    Implemented DeadlineTask

--- src/tickerPackage/DeadlineTask.java
+++ src/tickerPackage/DeadlineTask.java
@@ -1,5 +1,12 @@
 package tickerPackage;
 
-public class DeadlineTask {
-
+public class DeadlineTask extends Task {
+	Date endDate;
+	Time endTime;
+	
+	DeadlineTask(String description, Date endDate, Time endTime) {
+		super(description);
+		this.endDate = endDate;
+		this.endTime = endTime;
+	}
 }
