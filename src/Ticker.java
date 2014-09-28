ed57d31d132b55284b9d6355e729bf5172787723
Nicholette <nekonekonik@gmail.com>
2014-09-28 14:15:38 +0800

    added in initialisation of attributes

--- src/Ticker.java
+++ src/Ticker.java
@@ -9,18 +9,30 @@
  * This program assumes that:
  */
 
+/* This code is last edited by Nicholette 28/9/2014 */
+
 public class Ticker {
-	// Constants
-	private static Parser;
-	private static Logic;
-	//private static UI;
-	//private static Storage;
+	// Attributes
+	private static Ticker ticker;
+	private static Parser parser;
+	private static Logic logic;
+	//private static UI ui;
+	//private static Storage storage;
 	
 	public Ticker() {
 	}
 	
+	public Ticker getTicker() {
+		return ticker;
+	}
+	
 	public static void main(String[] args) {
-		
+		// Initialisation
+		ticker = new Ticker();
+		parser = new Parser();
+		logic = new Logic();
+		//ui = new UI();
+		//storage = new Storage();
 		
 	}
 	
