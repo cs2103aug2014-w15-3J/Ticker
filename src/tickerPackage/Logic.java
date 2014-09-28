3bccb855b0549ea0d7001404f309323c8db11525
Nicholette <nekonekonik@gmail.com>
2014-09-28 15:09:50 +0800

    Creation of Logic.java and implementation of delete method

--- src/tickerPackage/Logic.java
+++ src/tickerPackage/Logic.java
@@ -1,2 +1,58 @@
 package tickerPackage;
 
+import java.util.Vector;
+
+class Logic{
+	// Pointer to the Vector currently in display
+	Vector<Task> current;
+
+	// Temporary sorted storages
+	Vector<Task> sortedTime;
+	Vector<Task> sortedPriority;
+	Vector<Task> searchResults;
+
+	public Logic(){
+		// TODO Transfer data from storage
+
+		// STUB:
+		sortedTime = new Vector<Task>();
+		sortedPriority = new Vector<Task>();
+		searchResults = new Vector<Task>();
+
+		current = sortedTime;
+
+	}	
+
+	public boolean delete(int index) {
+		if (index >= 0 && index < current.size()) {
+			current.remove(index);
+			return true;
+		}
+		return false;
+	}
+
+	public boolean search(String str) {
+		// TODO Auto-generated method stub
+		System.out.println("search");
+		return false;
+	}
+
+	public boolean list() {
+		// TODO Auto-generated method stub
+		System.out.println("list");
+		return false;
+	}
+
+	public void edit(int index, boolean isAppending, String description) {
+		// TODO Auto-generated method stub
+		System.out.println("edit");
+	}
+
+
+
+	public void add(String description, Boolean isRepeating, Date sd, Date ed,
+			Time st, Time et) {
+		// TODO Auto-generated method stub
+		System.out.println("add");
+	}
+}
