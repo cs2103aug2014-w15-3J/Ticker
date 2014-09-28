c0abe8b7d2eea84bce2130eba014ef6af3862ecd
Nicholette <nekonekonik@gmail.com>
2014-09-28 13:46:34 +0800

    Creation of Ticker class

---
+++ src/Ticker.java
@@ -0,0 +1,27 @@
+/* Team ID: W15-3J
+ * Project name: Ticker
+ * Group Members: 
+ *
+ *
+ *
+ *
+ * Purpose:
+ * This program assumes that:
+ */
+
+public class Ticker {
+	// Constants
+	private static Parser;
+	private static Logic;
+	//private static UI;
+	//private static Storage;
+	
+	public Ticker() {
+	}
+	
+	public static void main(String[] args) {
+		
+		
+	}
+	
+}
