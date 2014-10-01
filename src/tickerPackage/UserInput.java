72874e52d783316b020a70eb052bf261fdcddc8c
Liu Kexin <kexinliu131@gmail.com>
2014-10-01 16:59:23 +0800

    added UserInput Class

---
+++ src/tickerPackage/UserInput.java
@@ -0,0 +1,19 @@
+package tickerPackage;
+
+public class UserInput {
+	
+	public String command;
+	public String description;
+	public Time startTime;
+	public Time endTime;
+	public Date endDate;
+	public Date startDate;
+	public boolean isRepeatingAppending;
+	public int index;
+	public int Priority;
+	
+	public UserInput(){
+		
+	}
+
+}
