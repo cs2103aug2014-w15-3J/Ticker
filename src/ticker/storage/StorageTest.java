87776cee2a5c904b2d7eebb80c35488f23fdfe70
jials <jiale@comp.nus.edu.sg>
2014-10-02 22:20:32 +0800

    Added Unit Testing for parseTimeIntoJSON

--- src/ticker/storage/StorageTest.java
+++ src/ticker/storage/StorageTest.java
@@ -1,10 +1,13 @@
 package ticker.storage;
 
 import static org.junit.Assert.*;
+import Storage;
 
+import org.json.JSONObject;
 import org.junit.Test;
 
 import tickerPackage.FloatingTask;
+import tickerPackage.Time;
 
 public class StorageTest {
 
@@ -15,6 +18,15 @@ public class StorageTest {
 		FloatingTask sample  = new FloatingTask(description);
 		String result = test.parseFloatingTaskIntoJSON(sample);
 		assertEquals(result, "{\"description\":\"DO HOMEWORK!!!\"}");
+	}
+	
+	@Test
+	public void testParseTimeIntoJSON() {
+		Storage test = new Storage();
+		String description = "DO HOMEWORK!!!";
+		Time sample  = new Time(11, 59);
+		JSONObject result = test.parseTimeIntoJSON(sample);
+		assertEquals(result.toString(), "{\"min\":59,\"hour\":11}");
 	}
 
 }
