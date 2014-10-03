783a2b3f15ecc6f6ab06a81ffa4319077e042d82
jials <jiale@comp.nus.edu.sg>
2014-10-03 09:07:25 +0800

    Revert "Added Unit Testing for parseTimedTaskIntoJSON"
    This reverts commit d2a810aeefef14430b1495febda5236b916f7dd4.

--- src/ticker/storage/StorageTest.java
+++ src/ticker/storage/StorageTest.java
@@ -1,6 +1,7 @@
 package ticker.storage;
 
 import static org.junit.Assert.*;
+import Storage;
 
 import org.json.JSONObject;
 import org.junit.Test;
@@ -8,36 +9,25 @@ import org.junit.Test;
 import tickerPackage.Date;
 import tickerPackage.FloatingTask;
 import tickerPackage.Time;
-import tickerPackage.TimedTask;
 
 public class StorageTest {
 
-	@Test
-	public void testParseTimedTaskIntoJSON() {
-		Storage test = new Storage();
-		String description = "DO HOMEWORK!!!";
-		TimedTask sample  = new TimedTask(description, new Date(2014,12,29), new Time(5,35), new Date(2015,1,1), new Time(13,45));
-		JSONObject result = test.parseTimedTaskIntoJSON(sample);
-		assertEquals("{\"startTime\":{\"min\":35,\"hour\":5},\"startDate\":{\"month\":12,\"year\":2014,\"date\":29},"
-						+ "\"description\":\"DO HOMEWORK!!!\",\"endDate\":{\"month\":1,\"year\":2015,\"date\":1},"
-						+ "\"endTime\":{\"min\":45,\"hour\":13}}", result.toString());
-	}
-	
 	@Test
 	public void testParseFloatingTaskIntoJSON() {
 		Storage test = new Storage();
 		String description = "DO HOMEWORK!!!";
 		FloatingTask sample  = new FloatingTask(description);
-		JSONObject result = test.parseFloatingTaskIntoJSON(sample);
-		assertEquals("{\"description\":\"DO HOMEWORK!!!\"}", result.toString());
+		String result = test.parseFloatingTaskIntoJSON(sample);
+		assertEquals(result, "{\"description\":\"DO HOMEWORK!!!\"}");
 	}
 	
 	@Test
 	public void testParseTimeIntoJSON() {
 		Storage test = new Storage();
+		String description = "DO HOMEWORK!!!";
 		Time sample  = new Time(11, 59);
 		JSONObject result = test.parseTimeIntoJSON(sample);
-		assertEquals("{\"min\":59,\"hour\":11}", result.toString());
+		assertEquals(result.toString(), "{\"min\":59,\"hour\":11}");
 	}
 	
 	@Test
@@ -45,7 +35,7 @@ public class StorageTest {
 		Storage test = new Storage();
 		Date sample  = new Date(2014, 12, 12);
 		JSONObject result = test.parseDateIntoJSON(sample);
-		assertEquals("{\"month\":12,\"year\":2014,\"date\":12}", result.toString());
+		assertEquals(result.toString(), "{\"month\":12,\"year\":2014,\"date\":12}");
 	}
 
 }
