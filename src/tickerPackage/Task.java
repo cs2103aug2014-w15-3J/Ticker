b1239e53e5d7a45a075bbf7cfbe27ca81dbf3d62
Nicholette <nekonekonik@gmail.com>
2014-10-07 23:52:22 +0800

    Saving Task class

--- src/tickerPackage/Task.java
+++ src/tickerPackage/Task.java
@@ -6,7 +6,7 @@ public class Task {
 	protected Time startTime;
 	protected Date endDate;
 	protected Time endTime;
-	
+
 	public Task(String description, Date startDate, Time startTime, Date endDate, Time endTime) {
 		this.description = description;
 		this.startDate = startDate;
@@ -14,50 +14,50 @@ public class Task {
 		this.endDate = endDate;
 		this.endTime = endTime;
 	}
-	
+
 	public String toString() {
 		return description;
 	}
-	
+
 	public String getDescription() {
 		return description;
 	}
-	
+
 	public Date getStartDate() {
 		return startDate;
 	}
-	
+
 	public Date getEndDate() {
 		return endDate;
 	}
-	
+
 	public Time getStartTime() {
 		return startTime;
 	}
-	
+
 	public Time getEndTime() {
 		return endTime;
 	}
-	
+
 	public void setDescription(String input) {
 		description = input;
 	}
-	
-	
+
+
 	public void setStartDate(Date input) {
 		startDate = input;
 	}
-	
+
 	public void setEndDate(Date input) {
 		endDate = input;
 	}
-	
+
 	public void setStartTime(Time input) {
 		startTime = input;
 	}
-	
+
 	public void setEndTime(Time input) {
 		endTime = input;
 	}
-	
+
 }
