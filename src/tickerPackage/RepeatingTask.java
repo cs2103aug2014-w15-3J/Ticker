35e920e4f8cfa5a0ce30b3829590c7a916539555
Nicholette <nekonekonik@gmail.com>
2014-10-09 19:55:56 +0800

    Edited repeatedTask toString. Still got error

--- src/tickerPackage/RepeatingTask.java
+++ src/tickerPackage/RepeatingTask.java
@@ -16,20 +16,21 @@ public class RepeatingTask extends Task {
 	}
 	
 	@Override
+	// TODO: error in printing time for startTime + endTime, endTime only
 	public String toString(){
 		String timing = "";
 		String temp = "";
 		
 		if(getStartTime()!=null&&getEndTime()!=null){
-			temp = " from " + getStartTime() + " to " + getEndTime();
+			temp = " from " + getStartTime() + " to " + getEndTime() + ", ";
 		}
 		
 		else if (getStartTime()!=null){
-			temp = " start at " + getStartTime();
+			temp = " start at " + getStartTime() + ", ";
 		}
 		
 		else if (getEndTime()!=null){
-			temp = " end at " + getEndTime();
+			temp = " end at " + getEndTime() + ", ";
 		}
 		
 		timing += temp + " ";
