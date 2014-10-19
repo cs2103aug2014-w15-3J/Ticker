f792c576a74c048b58a77dcf60287dfee6a45b74
Zhu Duoduo <dduozz@gmail.com>
2014-10-20 02:52:47 +0800

    the following changes: 1. display window is not directly editable now; 2. the window is not resizable; 3. tried to add in a logo but it doesn't show....

--- src/ticker/ui/TickerUI.java
+++ src/ticker/ui/TickerUI.java
@@ -11,10 +11,17 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -47,45 +54,64 @@ public class TickerUI extends Application {
 
 	public void start(Stage stage) {
         stage.setTitle("Ticker");
+        //AnchorPane main = new AnchorPane();
+        Group root = new Group();
+        
+        Image logo = new Image("file:logo.png");
+        ImageView imv = new ImageView();
+        imv.setImage(logo);
+        
+        
 		GridPane grid = new GridPane();
+		
 		
 		grid.setAlignment(Pos.TOP_CENTER);
 		
 		
-		grid.setStyle("-fx-background-color: white;");
+		grid.setStyle("-fx-background-color: azure;");
 		grid.setHgap(10);
 		grid.setVgap(10);
-		grid.setPadding(new Insets(10,10,10,10));
+		grid.setPadding(new Insets(60,15,10,15));
+		
 
-		Text enterCommand = new Text("Enter your command: ");
-		grid.add(enterCommand, 0, 2);
+		//Text enterCommand = new Text("Enter your command: ");
+		//grid.add(enterCommand, 0, 2);
 
 		final TextField command = new TextField();
-		grid.add(command, 1, 2);
+		grid.add(command, 0, 2);
+		
+		
 
-		Text showResult = new Text("Your To-dos: ");
-		grid.add(showResult, 0, 1);
+		//Text showResult = new Text("Your To-dos: ");
+		//grid.add(showResult, 0, 1);
 
 		final TextArea result = new TextArea();
 		result.setWrapText(true);
-		grid.add(result, 1, 1);
+		result.setPrefSize(470, 400);
+		result.setEditable(false);
+		
+		grid.add(result, 0, 1);
 		result.setText(list);
 
 		final Text feedback = new Text();
-		grid.add(feedback, 1, 3);
+		grid.add(feedback, 0, 3);
 
 		final Text currentTime = new Text();
 		grid.add(currentTime, 0, 0);
+		//grid.add(imv, 0, 0);
         
 		currentTime.setText(Time.getCurrentTime().toString() + " "+ Date.getCurrentDate().toString());
 		
 		
+		
 
-		Scene scene = new Scene(grid);
+		Scene scene = new Scene(grid, Color.AZURE);
 		
 		stage.setScene(scene);
-		stage.setWidth(700);
-		stage.setHeight(400);
+		stage.setMaxWidth(500);
+		stage.setMinWidth(500);
+		stage.setMaxHeight(600);
+		stage.setMinHeight(600);
 		
 		stage.show();
 
