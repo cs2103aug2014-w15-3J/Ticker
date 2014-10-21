14ba99e9bb8ca7d1fc4a76883ba50e5955a41bcd
Nicholette <nekonekonik@gmail.com>
2014-10-21 11:28:06 +0800

    Error with previous code. This is the working UI that accepts string

--- src/ticker/ui/TickerUI.java
+++ src/ticker/ui/TickerUI.java
@@ -1,11 +1,8 @@
 package ticker.ui;
 
 import java.util.Vector;
-import java.util.logging.*;
 
 import ticker.logic.Logic;
-import ticker.common.*;
-import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -21,16 +18,8 @@ import javafx.stage.Stage;
 public class TickerUI extends Application {
 	private static TickerUI ticker;
 	private Logic logic;
-	private String list;
-	private Vector<Task> tasksToBeShown; //possible list: sortedTime, sortedPriority, cmi, ticked, search
-	private String help;
-	private boolean displayHelp=false;
+	private static String list;
 	
-	AnimationTimer time;
-	
-	private static Logger logger = Logger.getLogger("UI");
-
-
 	public Logic getLogic() {
 		return logic;
 	}
@@ -41,114 +30,77 @@ public class TickerUI extends Application {
 		// Initialisation
 		ticker = this;
 		logic = new Logic(this);
-		help = help();
-	}
-
-
-	public void start(Stage stage) {
-        stage.setTitle("Ticker");
-		GridPane grid = new GridPane();
-		grid.setAlignment(Pos.TOP_CENTER);
-		grid.setHgap(10);
-		grid.setVgap(10);
-		grid.setPadding(new Insets(10,10,10,10));
-
-		Text enterCommand = new Text("Enter your command: ");
-		grid.add(enterCommand, 0, 2);
-
-		final TextField command = new TextField();
-		grid.add(command, 1, 2);
-
-		Text showResult = new Text("Your To-dos: ");
-		grid.add(showResult, 0, 1);
-
-		final TextArea result = new TextArea();
-		result.setWrapText(true);
-		grid.add(result, 1, 1);
-		result.setText(list);
-
-		final Text feedback = new Text();
-		grid.add(feedback, 1, 3);
-
-		final Text currentTime = new Text();
-		grid.add(currentTime, 0, 0);
-        
-		currentTime.setText(Time.getCurrentTime().toString() + " "+ Date.getCurrentDate().toString());
-
-		Scene scene = new Scene(grid);
-		stage.setScene(scene);
-		stage.setWidth(700);
-		stage.setHeight(400);
+		list = null;
 		
-		stage.show();
-
-		/*command.setOnAction(new EventHandler<ActionEvent>() 
-				{
-			public void handle(ActionEvent event) {
-				logger.log(Level.INFO, "user press enter once");
-				String cmd = command.getText();
-				assert !cmd.equals("");
-				command.clear();
-				//result.setText(cmd);
-				if(displayHelp == true) {
-					result.setText(help);
-					displayHelp = false;
-				} else {
-					result.setText(tasksToBeShown.toString());  // to be changed into Vector of Task
-				}
-				feedback.setText(logic.getLogic(cmd));
-				//feedback should disappear after one second
-
-				
-			}
-				});
-*/
 	}
 
+
+	public void start(Stage primaryStage) {
+        primaryStage.setTitle("Ticker");
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.TOP_LEFT);
+        grid.setHgap(0);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(10,10,10,10));
+
+        Text enterCommand = new Text("Enter your command: ");
+        grid.add(enterCommand, 0, 2);
+
+        final TextField command = new TextField();
+        grid.add(command, 1, 2);
+        
+        Text showResult = new Text("Your To-dos: ");
+        grid.add(showResult, 0, 1);
+
+        final TextArea result = new TextArea();
+        result.setWrapText(true);
+        grid.add(result, 1, 1);
+        
+        final Text feedback = new Text();
+        grid.add(feedback, 1, 3);
+
+        primaryStage.show();
+
+
+        Scene scene = new Scene(grid, 650, 300);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+       
+        command.setOnAction(new EventHandler<ActionEvent>() 
+        		{
+        	public void handle(ActionEvent event) {
+        		//System.out.println("Hello World");
+        		String cmd = command.getText();
+                //System.out.println(cmd);
+                command.clear();
+               // result.setText(cmd);
+                feedback.setText(logic.getLogic(cmd));
+                result.setText(list);
+        	}
+        		});
+        
+    }
+	
 	/*String printOut(Vector<Task> list) {
 		String output = null;
 		for(int i = 0; i <list.size(); i++) {
 			output.concat((i+1) + ". " + list.get(i).getDescription()+"\n");
 		}
-
+		
 		return output;
 	}*/
-
-	public void setHelp() {
-		 this.displayHelp = true;
+	
+	String manipulateString(String str) {
+		return str.substring(1, 5) + "\n" + str.substring(6, 8) + "\n" + "\n" + "\n" + str.substring(6, 8)+ "\n" + "\n" + "\n" + str.substring(6, 8)+"\n" + "\n" + "\n" + str.substring(6, 8)+"\n" + "\n" + "\n" + str.substring(6, 8) + "\n" + str.substring(6, 8) + "\n" + str.substring(6, 8)+ "\n" + str.substring(6, 8)+ "\n" + str.substring(6, 8);
 	}
-	private String help() {
-		String helpList = "";
-		helpList += "HELP FOR USING TICKER\n";
-		helpList += "-to add a task: add \"<task name>\" -st <start time> -sd <start date in dd/mm/yy format> "
-				+ "-et <end time> -ed <end date in dd/mm/yy format.\n";
-		helpList += "-to set a task to repeat, add the flag: -r\n";
-		helpList += "-to set a priority for a task, add the flag: to be continued\n";
-		helpList += "-to delete a task: delete <index of task>\n";
-		helpList += "-to edit a task: to be continued\n";
-		helpList += "-to sort the tasks according to time and date: list to be continued\n";
-		helpList += "-to sort the tasks according to priority: list to be continued\n";
-		helpList += "-to undo the last command: undo\n";
-		helpList += "-to redo the last undo: redo\n";
-		helpList += "-to mark a task as done: tick <index of task>\n";
-		helpList += "-to mark a task as cannot be done: cmi <index of task>\n";
-		//edit not done yet
-		return helpList;
-
-	}
-
+	
 	public void setList(String list) {
 		this.list = list;
 	}
-	//public void setList(Vector<Task> tasks) {
-		//this.tasksToBeShown = tasks;
-	//}
 
-
-	public static void main(String[] args) {
-		launch(args);
-		ticker = new TickerUI();
-
-
+    public static void main(String[] args) {
+    	launch(args);
+    	ticker = new TickerUI();
+    	
 	}
 }
