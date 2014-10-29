23cdd7f9e8cf0a2753ed6acdd31f7fec39725386
Zhu Duoduo <dduozz@gmail.com>
2014-10-30 01:14:36 +0800

    borderless GUI almost done: non-resizable, can close and drag around but cannot minimise -.-

--- src/ticker/ui/TickerUI.java
+++ src/ticker/ui/TickerUI.java
@@ -19,12 +19,15 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
@@ -37,6 +40,7 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.util.Duration;
 
 public class TickerUI extends Application {
@@ -48,10 +52,11 @@ public class TickerUI extends Application {
 	private String help;
 	private boolean displayHelp=false;
 
-	
+	double initialX, initialY;
+
 
 	private static Logger logger = Logger.getLogger("UI");
-	
+
 
 
 	public Logic getLogic() {
@@ -70,22 +75,24 @@ public class TickerUI extends Application {
 
 
 	public void start(Stage stage) {
+		stage = new Stage(StageStyle.UNDECORATED);
 		//overall structure: root.add background pic, logo, display box, command box, feedback line
 
-		stage.setTitle("Ticker");
 		Group root = new Group();
 
 		//background pic
-		Image background = new Image("ticker/ui/back.png", true);
+		Image background = new Image("ticker/ui/background.png", true);
 		ImageView imv1 = new ImageView();
 		imv1.setImage(background);
-		imv1.setFitWidth(500);
-		imv1.setPreserveRatio(true);
+		//imv1.setFitWidth(500);
+		imv1.setFitHeight(650);
+		imv1.setFitWidth(490);
 		imv1.setX(0);
 		imv1.setY(0);
 		imv1.setSmooth(true);
 		imv1.setCache(true);
 		root.getChildren().add(imv1);
+		addDragListeners(imv1);
 
 		//load the image of the logo
 		Image logo = new Image("ticker/ui/logo2.png", true);
@@ -94,7 +101,7 @@ public class TickerUI extends Application {
 		imv2.setFitWidth(130);
 		imv2.setPreserveRatio(true);
 		imv2.setX(15);
-		imv2.setY(20);
+		imv2.setY(25);
 		imv2.setSmooth(true);
 		imv2.setCache(true);
 		root.getChildren().add(imv2);
@@ -104,57 +111,43 @@ public class TickerUI extends Application {
 		command.setPromptText("Enter your command here...");
 		command.setPrefSize(450, 30);
 		//command.setMaxWidth(440);
-		command.setLayoutX(15);
-		command.setLayoutY(540);
+		command.setLayoutX(20);
+		command.setLayoutY(580);
 		root.getChildren().add(command);
 
 		//display console
-		final TextArea result = new TextArea();
-		//result.setLayoutX(15);
-		//result.setLayoutY(80);
-		result.setWrapText(true);
-		result.setPrefSize(450, 450);
-		result.setEditable(false);
-		result.setOpacity(0.8);
-		result.setText(list);
-		//root.getChildren().add(result);
-		
-		//an alternative of the above display console
-		//GridPane chart = new GridPane();
-		
+
 		ScrollPane sp = new ScrollPane();
-		sp.setPrefSize(450, 450);
-		sp.setLayoutX(15);
-		sp.setLayoutY(80);
+		sp.setPrefSize(450, 460);
+		sp.setLayoutX(20);
+		sp.setLayoutY(110);
 		sp.setContent(chart);
 		sp.setOpacity(0.8);
-		
-		//displayTasks();
-		
+
 		root.getChildren().add(sp); 
-		
-		
+
+
 		//add the header for the chart
-		
-		
-		
+
+
+
 
 		//feedback area
 		final Text feedback = new Text();
-		feedback.setLayoutX(17);
-		feedback.setLayoutY(590);
+		feedback.setLayoutX(22);
+		feedback.setLayoutY(628);
 		feedback.setFill(Color.BISQUE);
 		//feedback.setText("The command you entered is invalid");
 		root.getChildren().add(feedback);
 
-        //add the display of the current time
+		//add the display of the current time
 		/*Calendar c = Calendar.getInstance();
 		GridPane clock = new GridPane();
-		
+
 		final Text currentHour = new Text();
 		currentHour.setText(Integer.toString(Time.getCurrentTime().getHour()));
 		currentHour.setFill(Color.WHITE);
-		
+
 		final Text currentMin = new Text();
 		String min = "";
 		if(c.get(Calendar.MINUTE) < 10) {
@@ -164,7 +157,7 @@ public class TickerUI extends Application {
 		}
 		currentMin.setText(min);
 		currentMin.setFill(Color.WHITE);
-		
+
 		final Text currentSec = new Text();
 		String sec = "";
 		if(c.get(Calendar.SECOND) < 10) {
@@ -174,13 +167,13 @@ public class TickerUI extends Application {
 		}
 		currentSec.setText(sec);
 		currentSec.setFill(Color.WHITE);
-		
+
 		final Text colon1 = new Text(":");
 		colon1.setFill(Color.WHITE);
-		
+
 		final Text colon2 = new Text(":");
 		colon2.setFill(Color.WHITE);
-		
+
 		clock.add(currentHour, 0, 0);
 		clock.add(colon1, 1, 0);
 		clock.add(currentMin, 2, 0);
@@ -188,12 +181,12 @@ public class TickerUI extends Application {
 		clock.add(currentSec, 4, 0);
 		clock.setLayoutX(380);
 		clock.setLayoutY(40);
-		
-		
+
+
 
 		final Timeline time = new Timeline();
 		time.setCycleCount(Timeline.INDEFINITE);
-		
+
 		root.getChildren().add(clock);*/
 		// redo?
 		/*String dTime;
@@ -212,23 +205,98 @@ public class TickerUI extends Application {
 		}.play();*/
 		//String time;
 
+		//add the minimise and close button
+		Image min = new Image("ticker/ui/minimise2.png", true);
+		ImageView imv3 = new ImageView();
+		imv3.setImage(min);
+		imv3.setFitWidth(18);
+		imv3.setPreserveRatio(true);
+		imv3.setX(438);
+		imv3.setY(15);
+		imv3.setSmooth(true);
+		imv3.setCache(true);
+		root.getChildren().add(imv3);
+
+		Image close = new Image("ticker/ui/close2.png", true);
+		ImageView imv4 = new ImageView();
+		imv4.setImage(close);
+		imv4.setFitWidth(20);
+		imv4.setPreserveRatio(true);
+		imv4.setFitWidth(18);
+		imv4.setX(464);
+		imv4.setY(7);
+		imv4.setSmooth(true);
+		imv4.setCache(true);
+		root.getChildren().add(imv4);
+
+		//add in the mouseOn status of those two buttons
+		Image min_ = new Image("ticker/ui/minimiseMouseOn.png", true);
+		Image close_ = new Image("ticker/ui/closeMouseOn.png", true);
+
+		//for the close button
+		imv4.setOnMouseEntered(new EventHandler<MouseEvent>() {
+
+			public void handle(MouseEvent evt) {
+				imv4.setImage(close_);
+			}
+		});
+		imv4.setOnMouseExited(new EventHandler<MouseEvent>() {
+
+			public void handle(MouseEvent evt) {
+				imv4.setImage(close);
+			}
+		});
+		imv4.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+			public void handle(MouseEvent evt) {
+				System.exit(0);
+				//stage.close();
+			}
+		});
+		
+		
+		//TODO figure out how to solve this problem and get minimise button to work
+		//for the minimise button
+		imv3.setOnMouseEntered(new EventHandler<MouseEvent>() {
+
+			public void handle(MouseEvent evt) {
+				imv3.setY(12);
+				imv3.setImage(min_);
+			}
+		});
+		imv3.setOnMouseExited(new EventHandler<MouseEvent>() {
+
+			public void handle(MouseEvent evt) {
+				imv3.setY(15);
+				imv3.setImage(min);
+			}
+		});
+		imv3.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+			public void handle(MouseEvent evt) {
+				stage.setIconified(true);
+			}
+		});
 
 		Scene scene = new Scene(root);
 
 		stage.setScene(scene);
-		stage.setMaxWidth(500);
-		stage.setMinWidth(500);
+		stage.setMaxWidth(490);
+		stage.setMinWidth(490);
 		stage.setMaxHeight(650);
 		stage.setMinHeight(650);
+		//System.out.println(stage.getWidth());
 
 		stage.show();
 		displayTasks();
+		
 		logger.log(Level.INFO, "the stage is set up");
 
 		command.setOnAction(new EventHandler<ActionEvent>() 
 				{
 			public void handle(ActionEvent event) {
 				logger.log(Level.INFO, "user press enter once");
+
 				String cmd = command.getText();
 				assert !cmd.equals("");
 				command.clear();
@@ -246,7 +314,7 @@ public class TickerUI extends Application {
 					displayTasks();
 				}
 
-				//feedback fades off after 2 seconds
+				//feedback fades off after 5 seconds
 				FadeTransition ft = new FadeTransition(Duration.millis(5000), feedback);
 				ft.setFromValue(1.0);
 				ft.setToValue(0);
@@ -280,9 +348,7 @@ public class TickerUI extends Application {
 
 	}
 
-	//public void setList(String list) {
-	//	this.list = list;
-	//}
+
 	public void setList(Vector<Task> tasks) {
 		this.tasksToBeShown = tasks;
 	}
@@ -291,9 +357,8 @@ public class TickerUI extends Application {
 	public static void main(String[] args) {
 		launch(args);
 		ticker = new TickerUI();
-
 	}
-	
+
 	//a fake task list used for testing correctness of display
 	public void fillInTasks() {
 		Task task1 = new Task("do homework", new Date(2014,11,13), new Time(6, 0), null, null, 'A', false);
@@ -307,7 +372,7 @@ public class TickerUI extends Application {
 		tasksToBeShown.add(task4);
 		tasksToBeShown.add(task5);
 	}
-	
+
 	public void displayTasks() {
 		int prefHeight = 30;
 		int maxHeight = 100;
@@ -315,7 +380,7 @@ public class TickerUI extends Application {
 		int widthDes = 250;
 		int widthTime = 140;
 		for(int i = 0; i < tasksToBeShown.size(); i++ ) {
-			
+
 			HBox hb = new HBox(10);
 			hb.setPadding(new Insets(15, 10, 5, 10));
 
@@ -323,7 +388,7 @@ public class TickerUI extends Application {
 			Label index = new Label(""+(i+1)+".");
 			index.setPrefSize(widthIndex, prefHeight);
 			index.setAlignment(Pos.TOP_LEFT);
-			
+
 			//task description
 			Label description = new Label(tasksToBeShown.get(i).getDescription());
 			description.setMaxSize(widthDes, maxHeight);
@@ -335,17 +400,17 @@ public class TickerUI extends Application {
 			Date ed = tasksToBeShown.get(i).getEndDate();
 			Time st = tasksToBeShown.get(i).getStartTime();
 			Time et = tasksToBeShown.get(i).getEndTime();
-			
-String SD, ED, ST, ET;
-			
+
+			String SD, ED, ST, ET;
+
 			SD = (sd==null)? "" : sd.toString();
 			ED = (ed==null)? "" : ed.toString();
 			ST = (st==null)? "" : st.toString();
 			ET = (et==null)? "" : et.toString();
-			
-		
-			
-			
+
+
+
+
 			if(sd==null && st==null && ed==null && et==null) {
 				hb.getChildren().addAll(index, description);
 			}
@@ -356,8 +421,8 @@ String SD, ED, ST, ET;
 				hb.getChildren().addAll(index, description, start);
 			}
 			else if (sd==null && st==null) {
-			    Label end = new Label("End: " + ET + " " + ED);
-			    end.setMaxSize(widthTime, prefHeight);
+				Label end = new Label("End: " + ET + " " + ED);
+				end.setMaxSize(widthTime, prefHeight);
 				end.setAlignment(Pos.TOP_LEFT);
 				hb.getChildren().addAll(index, description, end);
 			}
@@ -365,21 +430,53 @@ String SD, ED, ST, ET;
 				Label start = new Label("Start: " + ST + " " + SD);
 				//start.setMaxSize(widthTime, prefHeight);
 				start.setAlignment(Pos.TOP_LEFT);
-				
+
 				Label end = new Label("End: " + ET + " " + ED);
 				//end.setMaxSize(widthTime, prefHeight);
 				end.setAlignment(Pos.TOP_LEFT);
-				
+
 				VBox time = new VBox(5);
 				time.getChildren().add(start);
 				time.getChildren().add(end);
 				time.setPrefSize(widthTime, prefHeight);
 				hb.getChildren().addAll(index, description, time);
 			}
-			
-	
+
+
 			chart.add(hb, 0, i);
 		}
-		
+
+	}
+
+
+	private void addDragListeners(final ImageView node){
+
+		node.setOnMousePressed(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent me) {
+				if(me.getButton()!=MouseButton.MIDDLE) {
+					initialX = me.getSceneX();
+					initialY = me.getSceneY();
+				}
+				else
+				{
+					node.getScene().getWindow().centerOnScreen();
+					initialX = node.getScene().getWindow().getX();
+					initialY = node.getScene().getWindow().getY();
+				}
+
+			}
+		});
+
+		node.setOnMouseDragged(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent me) {
+				if(me.getButton()!=MouseButton.MIDDLE)
+				{
+					node.getScene().getWindow().setX( me.getScreenX() - initialX );
+					node.getScene().getWindow().setY( me.getScreenY() - initialY);
+				}
+			}
+		});
 	}
 }
