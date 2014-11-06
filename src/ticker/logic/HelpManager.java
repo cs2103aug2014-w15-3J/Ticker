3881c2b2f3ca8ef8ef0a9242fe6e6ad7e79c6a86
jiale <jiale@comp.nus.edu.sg>
2014-11-06 11:30:32 +0800

    Added HelpManager

---
+++ src/ticker/logic/HelpManager.java
@@ -0,0 +1,142 @@
+package ticker.logic;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Vector;
+
+import uk.ac.shef.wit.simmetrics.similaritymetrics.AbstractStringMetric;
+import uk.ac.shef.wit.simmetrics.similaritymetrics.MongeElkan;
+import uk.ac.shef.wit.simmetrics.similaritymetrics.SmithWaterman;
+import uk.ac.shef.wit.simmetrics.similaritymetrics.SmithWatermanGotoh;
+import uk.ac.shef.wit.simmetrics.similaritymetrics.SmithWatermanGotohWindowedAffine;
+
+public class HelpManager {
+	private Vector<StringMatch> matchList;
+	private String key;
+	private HashMap<String, String> helpList;
+	private String[] commandListSet;
+
+	
+	private static final String COMMAND_HELP = "help";
+	private static final String COMMAND_UNTICK = "untick";
+	private static final String COMMAND_TICK = "tick";
+	private static final String COMMAND_REDO = "redo";
+	private static final String COMMAND_UNDO = "undo";
+	private static final String COMMAND_UNKIV = "unkiv";
+	private static final String COMMAND_KIV = "kiv";
+	private static final String COMMAND_ADD = "add";
+	private static final String COMMAND_EDIT = "edit";
+	private static final String COMMAND_LIST = "list";
+	private static final String COMMAND_CLEAR = "clear";
+	private static final String COMMAND_DELETE = "delete";
+	private static final String COMMAND_SEARCH = "search";
+
+	private static final String MESSAGE_HELP = "help";
+	private static final String MESSAGE_UNTICK = "untick <index>";
+	private static final String MESSAGE_TICK = "tick <index>";
+	private static final String MESSAGE_REDO = "redo";
+	private static final String MESSAGE_UNDO = "undo";
+	private static final String MESSAGE_UNKIV = "unkiv <index>";
+	private static final String MESSAGE_KIV = "kiv <index>";
+	private static final String MESSAGE_ADD = "add <description> <startDate>-<endDate> <startTime>-<endTime>";
+	private static final String MESSAGE_EDIT = "edit <index>";
+	private static final String MESSAGE_LIST = "list <listName>";
+	private static final String MESSAGE_CLEAR = "clear";
+	private static final String MESSAGE_DELETE = "delete <index>";
+	private static final String MESSAGE_SEARCH = "search <words>";
+
+	//@author A0116673A
+
+	public HelpManager() {
+		helpList = new HashMap<String, String>();
+		matchList = new Vector<StringMatch>();
+		initHelpList();
+		initCommandListSet();
+	}
+
+	/**
+	 *	Initialise the helplist with a list of available commands and corresponding
+	 *	help messages
+	 */
+	private void initHelpList() {
+		helpList.put(COMMAND_HELP, MESSAGE_HELP);
+		helpList.put(COMMAND_UNTICK, MESSAGE_UNTICK);
+		helpList.put(COMMAND_TICK, MESSAGE_TICK);
+		helpList.put(COMMAND_REDO, MESSAGE_REDO);
+		helpList.put(COMMAND_UNDO, MESSAGE_UNDO);
+		helpList.put(COMMAND_UNKIV, MESSAGE_UNKIV);
+		helpList.put(COMMAND_KIV, MESSAGE_KIV);
+		helpList.put(COMMAND_ADD, MESSAGE_ADD);
+		helpList.put(COMMAND_EDIT, MESSAGE_EDIT);
+		helpList.put(COMMAND_LIST, MESSAGE_LIST);
+		helpList.put(COMMAND_CLEAR, MESSAGE_CLEAR);
+		helpList.put(COMMAND_DELETE, MESSAGE_DELETE);
+		helpList.put(COMMAND_SEARCH, MESSAGE_SEARCH);
+	}
+
+	/**
+	 *	Initialise the commandListSet with a list of commands
+	*/
+	private void initCommandListSet() {
+		commandListSet = helpList.keySet().toArray(new String[0]); 
+	}
+
+	/**
+	 * This method return most likely help message that the user needs based on 
+	 * current input in the textfile
+	 *
+	 * @param   key       		current user input in textfield
+	 * @return     most likely help message based on the first word of user input
+	 */
+	public String getHelp(String key){
+		Vector<String> temp = new Vector<String>();
+		matchList.removeAllElements();
+
+		String firstWordKey = key.split(" ")[0];
+
+		int i = 0;	
+		for (String command: commandListSet) {
+			float score = getMatchLikelyhood(key.toLowerCase(), command);
+			matchList.add(new StringMatch(i, score));
+			i++;
+		}
+		
+		Collections.sort(matchList, new StringMatchComparator());
+
+		for (StringMatch sm : matchList) {
+			if (sm.getSimilarityScore() < 65.0) {
+				break;
+			}
+			temp.add(commandListSet[sm.getIndex()]);
+		}
+		
+		return helpList.get(temp.get(0));
+	}
+
+	/**
+	 * This method calculates the similarity index between two input strings based 
+	 * on the algorithms provided by Simmetrics library
+	 *
+	 * @param str1		input string 1
+	 * @param str2    	input string 2
+	 * @return     the similarity index between str1 and str2
+	 * @throws Error  If commandType is unidentified.
+	 */
+	private static float getMatchLikelyhood(final String str1, final String str2) {
+		AbstractStringMetric metric;
+		float avg = 0F, result = 0F;
+		metric = new SmithWaterman();
+		result = metric.getSimilarity(str1, str2);
+		avg += result;
+		metric = new SmithWatermanGotoh();
+		result = metric.getSimilarity(str1, str2);
+		avg += result;
+		metric = new SmithWatermanGotohWindowedAffine();
+		result = metric.getSimilarity(str1, str2);
+		avg += result;
+		metric = new MongeElkan();
+		result = metric.getSimilarity(str1, str2);
+		avg += result;
+		return (avg / 4.0F) * 100.0F;
+	}
+}	
