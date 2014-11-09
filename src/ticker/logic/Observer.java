b725fa862c1e8607a233f4d9ce3ef3f02b64efaa
Nicholette <nekonekonik@gmail.com>
2014-11-09 17:38:04 +0800

    Removed name in comment

--- src/ticker/logic/Observer.java
+++ src/ticker/logic/Observer.java
@@ -1,9 +1,6 @@
 //@author A0114535M
 
 /* Team ID: W15-3J
- * Name: Li Jia'En, Nicholette
- * Matric Number: A0114535M
- * 
  * Project Title: Ticker
  * Interface: Observer
  * Description: This interface allows any type or number of UI to observe the Logic class.
