6a419cf5de0298ae73af5a0384817c237778f989
Liu Kexin <kexinliu131@gmail.com>
2014-11-10 18:31:32 +0800

    TimedTask toString() changed

--- src/ticker/common/TimedTask.java
+++ src/ticker/common/TimedTask.java
@@ -5,6 +5,10 @@ public class TimedTask extends Task {
 	// CONSTANTS
 	private static final int EQUAL = 0;
 	private static final int SMALLER = -1;
+	private static final String FROM = " from";
+	private static final String SPACE = " ";
+	private static final String COMMA = ", ";
+	private static final String TO = " to";
 
 	// ATTRIBUTES
 	public final int id = 2; // used in storage
@@ -45,18 +49,37 @@ public class TimedTask extends Task {
 	 * This method prints the TimedTask.
 	 */
 	@Override
-	public String toString() {
-		String timing = "";
-
-		timing += (!(getStartTime() == null && getStartDate() == null)) ? " from"
-				: "";
-		timing += (getStartDate() == null) ? "" : " " + getStartDate();
-		timing += (getStartTime() == null) ? "" : ", " + getStartTime();
-		timing += (!(getEndTime() == null && getEndDate() == null)) ? " to"
-				: "";
-		timing += (getEndDate() == null) ? "" : " " + getEndDate();
-		timing += (getEndTime() == null) ? "" : ", " + getEndTime();
-
-		return getDescription() + timing;
+	public String toString(){
+		String timing = SPACE;
+		
+		if (!(getStartTime() == null&&getStartDate() == null)){
+			timing += FROM;
+		}
+		
+		if (getStartDate() != null){
+			timing += SPACE;
+			timing += getStartDate();
+		}
+		
+		if (getStartTime() != null){
+			timing += COMMA;
+			timing += getStartTime();
+		}
+		
+		if(!(getEndTime() == null&&getEndDate() == null)){
+			timing += TO;
+		}
+		
+		if (getEndDate() != null){
+			timing += SPACE;
+			timing += getEndDate();
+		}
+		
+		if (getEndTime() != null){
+			timing += COMMA;
+			timing += getEndTime();
+		}
+		
+		return getDescription() + timing; 
 	}
 }
